4767649784ae6bd34ad547e666f77d3689d46724
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-06-14 12:34:15 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,4 +4,9 @@
 const myHeading = document.querySelector('h1');
  
 // Replace the heading text with Hello World!
+
 myHeading.textContent = 'Hello world!';
+function changeColor(newColor) {
+  var elem = document.getElementById('myPara');
+  elem.style.color = newColor;
+}
