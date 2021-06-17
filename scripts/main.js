1fb8cebe201f4dbb8e5da38bf3d488e2530d4be6
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-06-17 14:40:16 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,7 @@
 // A snippet of code to replace Mozilla is Cool with Hello World
  
 // Grab the heading in the document
-var myHeading = document.getElementByID('alpha');
- 
-// Replace the heading text with Hello World!
-
-myHeading.textContent = 'Hello world!';
+var myHeading = document.getElementByID('alpha').textContent="Hello World!";
 
 function changeColor(newColor) {
   var elem = document.getElementById('myPara');
