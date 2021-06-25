5e8445cf87c2797108fbcb8002aa875503c4936a
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-06-24 22:09:20 -0400

    Update index.js

--- flashcards/basic/scripts/index.js
+++ flashcards/basic/scripts/index.js
@@ -1,17 +1,23 @@
-class country {
+class question {
 	constructor (question, answer) {
 		this.question = question;
 		this.answer = answer;
+		this.correct = false;
 	}
 }
-let countries = [];
-countries.push(new country("What is the capital of Mexico?", "Mexico City"));
-countries.push(new country("What is the capital of Russia?", "Moscow"));
-countries.push(new country("What is the capital of Bangladesh?", "Dhaka"));
-countries.push(new country("What is the capital of Nigeria?", "Abuja"));
-countries.push(new country("What is the capital of Brazil?", "Brasilia"));
-countries.push(new country("What is the capital of Pakistan?", "Islamabad"));
-countries.push(new country("What is the capital of Indonesia?", "Jakarta"));
-countries.push(new country("What is the capital of the United States?", "Washington, D.C."));
-countries.push(new country("What is the capital of India?", "New Delhi"));
-countries.push(new country("What is the capital of China?", "Beijing"));
+let quiz = [];
+quiz.push(new question('What is the capital of Mexico?', 'Mexico City'));
+quiz.push(new question('What is the capital of Russia?', 'Moscow'));
+quiz.push(new question('What is the capital of Bangladesh?', 'Dhaka'));
+quiz.push(new question('What is the capital of Nigeria?', 'Abuja'));
+quiz.push(new question('What is the capital of Brazil?', 'Brasilia'));
+quiz.push(new question('What is the capital of Pakistan?', 'Islamabad'));
+quiz.push(new question('What is the capital of Indonesia?', 'Jakarta'));
+quiz.push(new question('What is the capital of the United States?', 'Washington, D.C.'));
+quiz.push(new question('What is the capital of India?', 'New Delhi'));
+quiz.push(new question('What is the capital of China?', 'Beijing'));
+let i = 0;
+while (i < quiz.length)  {
+  console.log(quiz[i].question + ' ' + quiz[i].answer);
+  ++i
+}
