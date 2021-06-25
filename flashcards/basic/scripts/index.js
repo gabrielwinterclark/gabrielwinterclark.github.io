2c046374a80d128afc9bea11568e50fe40fcb8e9
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-06-24 22:35:40 -0400

    Update index.js

--- flashcards/basic/scripts/index.js
+++ flashcards/basic/scripts/index.js
@@ -9,16 +9,16 @@ class question {
 	}
 }
 let quizObjects = [];
-quizObjects.push(new question('What is the capital of Mexico?', 'Mexico City'));
-quizObjects.push(new question('What is the capital of Russia?', 'Moscow'));
-quizObjects.push(new question('What is the capital of Bangladesh?', 'Dhaka'));
-quizObjects.push(new question('What is the capital of Nigeria?', 'Abuja'));
-quizObjects.push(new question('What is the capital of Brazil?', 'Brasilia'));
-quizObjects.push(new question('What is the capital of Pakistan?', 'Islamabad'));
-quizObjects.push(new question('What is the capital of Indonesia?', 'Jakarta'));
-quizObjects.push(new question('What is the capital of the United States?', 'Washington, D.C.'));
-quizObjects.push(new question('What is the capital of India?', 'New Delhi'));
-quizObjects.push(new question('What is the capital of China?', 'Beijing'));
+quizObjects.push(new banana('What is the capital of Mexico?', 'Mexico City'));
+quizObjects.push(new banana('What is the capital of Russia?', 'Moscow'));
+quizObjects.push(new banana('What is the capital of Bangladesh?', 'Dhaka'));
+quizObjects.push(new banana('What is the capital of Nigeria?', 'Abuja'));
+quizObjects.push(new banana('What is the capital of Brazil?', 'Brasilia'));
+quizObjects.push(new banana('What is the capital of Pakistan?', 'Islamabad'));
+quizObjects.push(new banana('What is the capital of Indonesia?', 'Jakarta'));
+quizObjects.push(new banana('What is the capital of the United States?', 'Washington, D.C.'));
+quizObjects.push(new banana('What is the capital of India?', 'New Delhi'));
+quizObjects.push(new banana('What is the capital of China?', 'Beijing'));
 	let quizItems = ''
 for (country of quizObjects) {
 	quizItems += '<li>' + country.name + '</li>'
