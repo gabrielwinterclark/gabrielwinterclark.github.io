12b61ec3c77bfc38f348a8bb9ac057a3f0dc3e5d
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-06-26 16:49:52 -0400

    Update index.js

--- flashcards/basic/scripts/index.js
+++ flashcards/basic/scripts/index.js
@@ -14,3 +14,15 @@ quiz.push(new question('What is the capital of Bangladesh','Dhaka'));
 for (let i = 0; i < quiz.length; i++) {
   console.log(quiz[i]);
 }
+var questionOne = document.getElementById(questionOne)
+var questionTwo = document.getElementById(questionTwo)
+var questionThree = document.getElementbyID(questionThree)
+var answerOne = document.getElementbyID(answerOne)
+var answerTwo = document.getElementbyID(answerTwo)
+var answerThree = document.getElementbyID(answerThree)
+				       
+questionOne.addEventListener("click", myFunction);
+
+function myFunction() {
+  alert ("Test");
+} 
