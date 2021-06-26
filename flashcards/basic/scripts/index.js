848e42c8e38a3ba4966c67ba8a0aa9ef1fd06922
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-06-26 16:02:34 -0400

    Update index.js

--- flashcards/basic/scripts/index.js
+++ flashcards/basic/scripts/index.js
@@ -9,4 +9,10 @@ let quiz = [];
 quiz.push(new question('What is the capital of Mexico?','Mexico City'));
 quiz.push(new question('What is the capital of Russia?','Moscow'));
 quiz.push(new question('What is the capital of Bangladesh','Dhaka'));
-console.log(hi)
+
+// Loop through the array printing out the question and the answer
+let i = 0;
+while (i < quiz.length)  {
+  console.log(quiz[i].question + ' ' + quiz[i].answer);
+  ++i
+}
