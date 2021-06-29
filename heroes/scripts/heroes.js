4670c888bf63241fb356e48b71b47a47b89a9419
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-06-29 13:03:37 -0400

    Update heroes.js

--- heroes/scripts/heroes.js
+++ heroes/scripts/heroes.js
@@ -3,3 +3,5 @@ const header = document.querySelector('header');
 
 //Grab a reference to the section tag in the HTML file
 const section = document.querySelector('section');
+
+let requestURL = 'https://gabrielwinterclark.github.io/heroes/superheroes.json';
