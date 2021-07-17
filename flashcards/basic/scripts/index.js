12fa0bf5f00a114a7088aad2f875c45a3b4ed5a4
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-07-17 00:04:49 -0400

    Update index.js

--- flashcards/basic/scripts/index.js
+++ flashcards/basic/scripts/index.js
@@ -15,30 +15,37 @@ quiz.push(new question("What is the capital of Mexico?","Mexico City"));
 quiz.push(new question("What is the capital of Russia?","Moscow"));
 quiz.push(new question("What is the capital of Bangladesh?","Dhaka"));
 
-//declare a variable called "questionNumber" and set it equal to 1 - so we can access this variable outside the if/else code below
+//declare a variable called "questionNumber" and set it equal to 0 - so we can access this variable outside the if/else code below
 let questionNumber = 0;
 
-//create a function that 
+//add a handler so that when the user clicks, we start the function below
+document.querySelector('html').onclick = function() {
+	showQuestion(questionNumber);
+}
+
+//create a function that displays questions one at a time
 function showQuestion(i) {
   if (questionNumber < quiz.length) {
   	console.log(quiz[i].question);
+	  //add another handler event so that when the user clicks, we are directed to a new function "showAnswer" that will show the answer - see below
   	document.querySelector('html').onclick = function() {
 		showAnswer(i);
   	}
   }
+	//if the user does not click or if we've already gone through the questions, we return. since the questionNumber doesn't change unless the user clicks and we go to "showAnswer", nothing happens.
   else {
 	return;
   }
 }
 
+//make a function that will show the answer of each question after the user clicks
 function showAnswer(i) {
   console.log(quiz[i].answer);
+
+//add one to questionNumber so that we can move through the questions
   ++questionNumber;
+//add another handler so that when the user clicks, the next question is shown and we move back to the showQuestion function
   document.querySelector('html').onclick = function() {
 	  showQuestion(questionNumber);
   }
 }	
-
-document.querySelector('html').onclick = function() {
-	showQuestion(questionNumber);
-}
