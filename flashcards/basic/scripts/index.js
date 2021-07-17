2c609d2079d005e5714466ebf9c6e58102a58516
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-07-16 22:50:45 -0400

    Update index.js

--- flashcards/basic/scripts/index.js
+++ flashcards/basic/scripts/index.js
@@ -15,53 +15,7 @@ quiz.push(new question("What is the capital of Mexico?","Mexico City"));
 quiz.push(new question("What is the capital of Russia?","Moscow"));
 quiz.push(new question("What is the capital of Bangladesh?","Dhaka"));
 
-//loop through the array printing out the question and the answer in the console
+//loop through the array printing out the question in the console
 for (let i = 0; i < quiz.length; i++) {
   console.log(quiz[i].question);
 }
-
-//add a listener event for each question to display the answer when the question is clicked and display the next button 				       
-
-questionOne.addEventListener("click", myFunctionOne);
-
-function myFunctionOne() {
-  answerOne.style.visibility = 'visible';
-  nextOne.style.visibility = 'visible';	
-  console.log(quiz[0].answer);
-}
-
-questionTwo.addEventListener("click", myFunctionTwo);
-
-function myFunctionTwo() {
-  answerTwo.style.visibility = 'visible'
-  nextTwo.style.visibility = 'visible';
-  console.log(quiz[1].answer);
-}
-	
-questionThree.addEventListener("click", myFunctionThree);
-
-function myFunctionThree() {
-  answerThree.style.visibility = 'visible';
-  nextTwo.style.visibility = 'hidden';
-  console.log(quiz[2].answer)
-}
-
-//add listener event to next buttons so that the user can proceed to the next question when they click next
-
-nextTwo.addEventListener("click", myNextTwoFunction);
-
-function myNextTwoFunction( ) {
-	nextTwo.style.visibility = 'hidden';
-	questionTwo.style.visibility = 'hidden';
-	answerTwo.style.visibility = 'hidden';
-	questionThree.style.visibility = 'visible';
-}
-
-nextOne.addEventListener("click", myNextOneFunction);
-
-function myNextOneFunction( ) {
-	nextOne.style.visibility = 'hidden';
-	questionOne.style.visibility = 'hidden';
-	answerOne.style.visibility = 'hidden';
-	questionTwo.style.visibility = 'visible';
-}	
