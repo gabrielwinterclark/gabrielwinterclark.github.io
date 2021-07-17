2cebec151dd9c3fd9f1ea712ccf490a26f6cc95c
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-07-17 19:08:45 -0400

    Update index.js

--- flashcards/clickandhover/scripts/index.js
+++ flashcards/clickandhover/scripts/index.js
@@ -18,18 +18,10 @@ quiz.push(new question("What is the capital of Bangladesh?","Dhaka"));
 //declare a variable called "questionNumber" and set it equal to 0 - so we can access this variable outside the if/else code below
 let questionNumber = 0;
 
-//add a handler so that when the user clicks, we start the function below
-document.querySelector('html').onclick = function() {
-	showQuestion(questionNumber);
-}
-
 //create a function that displays questions one at a time
 function showQuestion(i) {
   if (questionNumber < quiz.length) {
-  	console.log(quiz[i].question);
-	  //add another handler event so that when the user clicks, we are directed to a new function "showAnswer" that will show the answer - see below
-  	document.querySelector('html').onclick = function() {
-		showAnswer(i);
+	document.getElementById("question") = quiz[i].question;
   	}
   }
 	//if the user does not click or if we've already gone through the questions, we return. since the questionNumber doesn't change unless the user clicks and we go to "showAnswer", nothing happens.
