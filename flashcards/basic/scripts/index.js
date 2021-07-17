735b4a2ca546a6d5884840ad1a2c698ff9e5465c
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-07-16 23:58:38 -0400

    Update index.js

--- flashcards/basic/scripts/index.js
+++ flashcards/basic/scripts/index.js
@@ -15,10 +15,12 @@ quiz.push(new question("What is the capital of Mexico?","Mexico City"));
 quiz.push(new question("What is the capital of Russia?","Moscow"));
 quiz.push(new question("What is the capital of Bangladesh?","Dhaka"));
 
-let questionNumber = 1;
+//declare a variable called "questionNumber" and set it equal to 1 - so we can access this variable outside the if/else code below
+let questionNumber = 0;
 
+//create a function that 
 function showQuestion(i) {
-  if (questionNumber <= quiz.length) {
+  if (questionNumber < quiz.length) {
   	console.log(quiz[i].question);
   	document.querySelector('html').onclick = function() {
 		showAnswer(i);
