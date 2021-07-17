afd336ab0943e746404dcc1975e53cbfb2213885
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-07-17 19:09:47 -0400

    Update index.js

--- flashcards/clickandhover/scripts/index.js
+++ flashcards/clickandhover/scripts/index.js
@@ -21,7 +21,7 @@ let questionNumber = 0;
 //create a function that displays questions one at a time
 function showQuestion(i) {
   if (questionNumber < quiz.length) {
-	document.getElementById("question") = quiz[i].question;
+	document.getElementById("question").innerHTML = quiz[i].question;
   	}
   }
 	//if the user does not click or if we've already gone through the questions, we return. since the questionNumber doesn't change unless the user clicks and we go to "showAnswer", nothing happens.
