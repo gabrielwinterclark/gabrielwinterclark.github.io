0eb43cd574f909e4c8db6833ae62921729af52ab
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-07-17 20:34:39 -0400

    Update index.js

--- flashcards/clickandhover/scripts/index.js
+++ flashcards/clickandhover/scripts/index.js
@@ -15,10 +15,8 @@ quiz.push(new question("What is the capital of Mexico?","Mexico City"));
 quiz.push(new question("What is the capital of Russia?","Moscow"));
 quiz.push(new question("What is the capital of Bangladesh?","Dhaka"));
 
-//declare a variable called "questionNumber" and set it equal to 0 - so we can access this variable outside the if/else code below
 let questionNumber = 0;
 
-//create a function that displays questions one at a time
 function showQuestion(i) {
   document.getElementById("answer").innerHTML = "";
   if (questionNumber < quiz.length) {
@@ -30,20 +28,17 @@ function showQuestion(i) {
   }
 }
 
-//make a function that will show the answer of each question after the user clicks
 function showAnswer(i) {
   document.getElementById("answer").innerHTML = quiz[i].answer;
-
-//add one to questionNumber so that we can move through the questions
   ++questionNumber;
   document.querySelector('html').onclick = function() {nextQuestion();
   }
 }
 
 function nextQuestion() {
-//add another handler so that when the user clicks, the next question is shown and we move back to the showQuestion function
   document.querySelector('html').onclick = function() {
 	  showQuestion(questionNumber);
+  document.getElementById("answer").innerHTML = "";
   }
 }	
 
